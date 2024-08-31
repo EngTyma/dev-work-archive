1a1ec63a83452ce5be86f952913a25b021c7ae9a
codefromhome1 <fatema14384@gmail.com>
2024-08-31 19:09:28 +0300

    initial commit

---
+++ 01. Beginners/test/assignment1.js
@@ -0,0 +1,37 @@
+
+/*=============================================
+=            Section comment block            =
+=============================================*/
+
+
+/**
+ *
+ * non-primitive types 
+1:objects
+ *
+ */
+
+let name = "ibtihala";
+let age = 25;
+let address = {
+    name:name,
+    age:age,
+};
+
+console.log(address.name);
+
+
+
+/**
+ *
+ * array
+ *
+ */
+const school = ['form1','form2','form3','form4'];
+console.log((school[0]));
+
+
+
+
+/*=====  End of Section comment block  ======*/
+
