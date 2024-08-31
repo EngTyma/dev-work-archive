3287cb10e5a339b4e1ae89c773d7afc15e0950b9
codefromhome1 <fatema14384@gmail.com>
2024-08-31 19:37:34 +0300

    initial commit

--- 01. Beginners/Night2.js
+++ 01. Beginners/Night2.js
@@ -4,13 +4,28 @@
 =============================================*/
 
 
-/* Arithmatic operators */
+/* Arithmetic Operators */
+
+// Assignment Operator
+let a = 5;
+let b = 5;
+let z = a+b;
+
+// Addition Operator
 let x = 10;
 let y = 5;
 console.log(x+y);
 
+// Multiplication Operator
+console.log(x*y);
 
+//increment Operator
+console.log(x++);
+console.log(x);
 
+//decrement Operator
+console.log(x--);
+console.log(x);
 
 /*=====  End of Section comment block  ======*/
 
