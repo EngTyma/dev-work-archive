9858aa53311e547951306c29cc5b26b3c8df9589
codefromhome1 <fatema14384@gmail.com>
2024-09-01 21:07:36 +0300

    initial commit

---
+++ 01. Beginners/Night3.js
@@ -0,0 +1,44 @@
+
+/*=============================================
+=            Section control flow            =
+=============================================*/
+
+
+/**
+ *
+ * if else stat...
+ *
+ */
+
+
+/**
+ *
+ * hour app
+if the hour is btw 6 adn 12 am - good morning
+else if the hour is btw 12 pm and 6 pm - good afternoon
+otherwise good evening
+ *
+ */
+
+let hour = 0;
+if(hour >=6 && hour <12)
+{
+    console.log("good morning");
+}
+
+else if(hour >=12 && hour < 18)
+{
+    console.log("Good Afternoon");
+}
+
+else{
+    console.log("Good Afternoon");
+}
+
+
+
+
+
+
+/*=====  End of Section control flow  ======*/
+
