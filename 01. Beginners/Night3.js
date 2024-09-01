53b26119eb57c060990f8077669994ae575e1313
codefromhome1 <fatema14384@gmail.com>
2024-09-01 21:13:25 +0300

    initial commit

--- 01. Beginners/Night3.js
+++ 01. Beginners/Night3.js
@@ -55,7 +55,38 @@ console.log(getGreeting());
 
 
 
+/**
+ *
+ * switch stat...
+ *
+ */
 
+let day = 0;
+switch(day) {
+    case 0:
+    console.log("Sunday");
+    break;
+    case 1:
+    console.log("Monday");
+    break;
+    case 2:
+    console.log("Tuesday");
+    break;
+    case 3:
+    console.log("Wednesday");
+    break;
+    case 4:
+    console.log("Thursday");
+    break;
+    case 5:
+    console.log("Friday");
+    break;
+    case 6:
+    console.log("Saturday");
+    break;
+    default:
+    console.log("Invalid day");
+    }
 
 
 /*=====  End of Section control flow  ======*/
