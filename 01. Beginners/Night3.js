373216330b1991c1c5c220a02afcebc55aa91e94
codefromhome1 <fatema14384@gmail.com>
2024-09-02 13:32:41 +0300

    initial commit

--- 01. Beginners/Night3.js
+++ 01. Beginners/Night3.js
@@ -1,4 +1,3 @@
-
 /*=============================================
 =            Section control flow            =
 =============================================*/
@@ -6,37 +5,30 @@
 
 /**
  *
- * if else stat...
+ * if-else statements
  *
  */
 
 
 /**
  *
- * hour app
-if the hour is btw 6 adn 12 am - good morning
-else if the hour is btw 12 pm and 6 pm - good afternoon
-otherwise good evening
+ * Hour app:
+ * If the hour is between 6 AM and 12 PM - Good morning
+ * If the hour is between 12 PM and 6 PM - Good afternoon
+ * Otherwise - Good evening
  *
  */
 
 let hour = 0;
-if(hour >=6 && hour <12)
-{
-    console.log("good morning");
+if (hour >= 6 && hour < 12) {
+    console.log("Good morning");
+} else if (hour >= 12 && hour < 18) {
+    console.log("Good afternoon");
+} else {
+    console.log("Good evening");
 }
 
-else if(hour >=12 && hour < 18)
-{
-    console.log("Good Afternoon");
-}
-
-else{
-    console.log("Good Afternoon");
-}
-
-//another way
-
+// Another way to get the greeting based on the current time
 function getGreeting() {
     const now = new Date();
     const hour = now.getHours();
@@ -54,55 +46,82 @@ function getGreeting() {
 console.log(getGreeting());
 
 
-
 /**
  *
- * switch stat...
+ * switch statements
  *
  */
 
 let day = 0;
-switch(day) {
+switch (day) {
     case 0:
-    console.log("Sunday");
-    break;
+        console.log("Sunday");
+        break;
     case 1:
-    console.log("Monday");
-    break;
+        console.log("Monday");
+        break;
     case 2:
-    console.log("Tuesday");
-    break;
+        console.log("Tuesday");
+        break;
     case 3:
-    console.log("Wednesday");
-    break;
+        console.log("Wednesday");
+        break;
     case 4:
-    console.log("Thursday");
-    break;
+        console.log("Thursday");
+        break;
     case 5:
-    console.log("Friday");
-    break;
+        console.log("Friday");
+        break;
     case 6:
-    console.log("Saturday");
-    break;
+        console.log("Saturday");
+        break;
     default:
-    console.log("Invalid day");
-    }
+        console.log("Invalid day");
+        break;
+}
 
-    let role;
-    switch(rorel){
-        case 'admin':
+let role = 'admin'; // Added initialization for `role` variable
+switch (role) {
+    case 'admin':
         console.log('Admin');
         break;
-        case 'moderator':
+    case 'moderator':
         console.log('Moderator');
         break;
-        case 'user':
+    case 'user':
         console.log('User');
         break;
-        default:
+    default:
         console.log('Invalid role');
-        
-    }
+        break;
+}
+
+// Alternative approach to determine role
+if (role === 'admin') {
+    console.log('Admin');
+} else if (role === 'moderator') {
+    console.log('Moderator');
+} else {
+    console.log('Invalid role');
+}
+
+
+/**
+ *
+ * Loops
+ *
+ */
+
+// For loop
+for (let i = 0; i < 5; i++) {
+    console.log(i);
+}
+
+// While loop
+let j = 0;
+while (j < 5) {
+    console.log(j);
+    j++;
+}
 
 /*=====  End of Section control flow  ======*/
-
