b19df8912ded1db9e277d9a5447117c3148818f2
codefromhome1 <fatema14384@gmail.com>
2024-09-04 20:18:38 +0300

    initial commit

---
+++ 01. Beginners/class5.js
@@ -0,0 +1,17 @@
+
+/*=============================================
+=            Section Functions           =
+=============================================*/
+
+//function declaration
+function work(){
+    console.log('work');
+}
+
+//function expresssion
+const run = function(){
+    console.log("Run");
+}
+run();
+/*=====  End of Section Functions  ======*/
+
