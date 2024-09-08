93feccc7b892c6756e54f55a501232d4180e52d4
codefromhome1 <fatema14384@gmail.com>
2024-09-08 20:48:21 +0300

    initial commit

--- 01. Beginners/class6.js
+++ 01. Beginners/class6.js
@@ -3,7 +3,19 @@
 =            Section Array           =
 =============================================*/
 
+const numbers = [1, 2, 3, 4, 5];
 
+//end 
+numbers.push(6);
+console.log(numbers)
+//begining
+numbers.shift(8,9)
+console.log(numbers)
+
+//midle
+
+numbers.splice(2,2,6,7,8);
+console.log(numbers)
 
 /*=====  End of Section Array  ======*/
 
