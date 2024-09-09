45f91019f3ee345e6429828b1b2db0ab87c7ac38
codefromhome1 <fatema14384@gmail.com>
2024-09-09 08:31:50 +0300

    initial commit

--- 01. Beginners/class6.js
+++ 01. Beginners/class6.js
@@ -7,14 +7,25 @@ const numbers = [1, 2, 3, 4, 5];
 
 //end 
 numbers.push(6);
-console.log(numbers)
+// console.log(numbers)
 //begining
 numbers.unshift(8,9)
-console.log(numbers)
+// console.log(numbers)
 
 //midle
 numbers.splice(0,0,10,11);
 console.log(numbers)
+
+
+/**
+ *
+ * finding element in an array (primitivite Types)
+ *
+ */
+
+const numbers2 = [1, 2, 3, 4, 5];
+console.log(numbers2.indexOf('a'));
+
 
 /*=====  End of Section Array  ======*/
 
