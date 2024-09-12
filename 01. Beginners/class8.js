0b2083483551e6bfd962fd9eaadeea49157cf142
codefromhome1 <fatema14384@gmail.com>
2024-09-12 17:21:14 +0300

    initial commit

--- 01. Beginners/class8.js
+++ 01. Beginners/class8.js
@@ -3,7 +3,8 @@
 =            Section DOM            =
 =============================================*/
 // seLECTING ELEMENT IN DOM
-
+const hello = document.getElementById('hello');
+hello.textContent = 'my world';
 
 /*=====  End of Section DOM  ======*/
 
