1393230b5ee18763765562bb1cdbc0d493e944d0
codefromhome1 <fatema14384@gmail.com>
2024-09-12 17:30:10 +0300

    initial commit

--- 01. Beginners/class8.js
+++ 01. Beginners/class8.js
@@ -6,5 +6,15 @@
 const hello = document.getElementById('hello');
 hello.textContent = 'my world';
 
+const tagname = document.getElementsByTagName('h1');
+
+tagname[0].style.backgroundColor = 'red';
+tagname[0].style.color = 'white';
+tagname[0].style.fontSize = '30px';
+tagname[0].style.textAlign = 'center';
+tagname[0].style.padding = '5px 10px';
+tagname[0].style.borderRadius = '5px';
+tagname[0].style.margin = '10px';
+
 /*=====  End of Section DOM  ======*/
 
