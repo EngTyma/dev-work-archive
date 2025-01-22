c401c83f421f092090ebd13b84264ccd2809c48d
codefromhome1 <fatema14384@gmail.com>
2025-01-22 16:05:41 +0300

    JS Variables

--- 02. Intermediate/class1.js
+++ 02. Intermediate/class1.js
@@ -2,6 +2,8 @@
 =            lesson1 Variables            =
 =============================================*/
 //   first example
+let nme = "Taslima";
+console.log(nme);
 
 console.log('Hello, world!');
 
@@ -9,20 +11,22 @@ console.log('Hello, world!');
 let name = 'ibtahala';
 console.log(name);
 
+
+
 // third example
 let age = 25;
 console.log(age);
 
 //fourth example.
-var x =10;
-var y=6;
-var z = x+y;
+var x = 10;
+var y = 6;
+var z = x + y;
 console.log(z);
 // fifth example
 
 /*=====  End of lesson1 Variables  ======*/
 
- 
+
 /*=============================================
 =            lesson2 constants        =
 =============================================*/
@@ -39,16 +43,16 @@ console.log(i);
 
 
 
- 
+
 /*=============================================
 =            lesson 3 primitive datatypes        =
 =============================================*/
 
-let j = "ibtisam";//string literal
-let k = 25;//number literal
-let check = true;//boolean literal
+let j = "ibtisam"; //string literal
+let k = 25; //number literal
+let check = true; //boolean literal
 // let num; //
-let num = undefined;//undefined literal
+let num = undefined; //undefined literal
 
 let color = null; //nul literal
 
@@ -75,16 +79,15 @@ console.log(person.name);
 console.log(person["name"]);
 
 const student = {
-    id:1,
-    name:"ibtihal",
+    id: 1,
+    name: "ibtihal",
 };
 
 //dot notation
 console.log(student.id);
 
 //bracket notation
-console.log(student["name"])
-;
+console.log(student["name"]);
 /*=====  End of lesson4  ======*/
 
 /*=============================================
@@ -112,8 +115,8 @@ const person1 = {
 };
 
 // Example usage
-console.log(person1.fullName());  // Output: Naghma Ahmed
-console.log(person1.describe());  // Output: Naghma Ahmed is 25 years old and is a student.
+console.log(person1.fullName()); // Output: Naghma Ahmed
+console.log(person1.describe()); // Output: Naghma Ahmed is 25 years old and is a student.
 
 
 /*=====  End of Assignment  ======*/
@@ -122,11 +125,11 @@ console.log(person1.describe());  // Output: Naghma Ahmed is 25 years old and is
 =            lesson 5 Arrays        =
 =============================================*/
 
-const cars = ['item1','item2','item3'];
+const cars = ['item1', 'item2', 'item3'];
 // cars = "items";
 console.log(cars[2]);
 
-let selectedcolor = ['red','black'];
+let selectedcolor = ['red', 'black'];
 selectedcolor = "yellow";
 // console.log(selectedcolor[0]);
 console.log(selectedcolor.length);
@@ -138,19 +141,18 @@ console.log(selectedcolor.length);
 =            lesson 6  functions      =
 =============================================*/
 
- // function expression performing task
- function greet(name,lname){
-    console.log("hello " + name,"", + lname);
+// function expression performing task
+function greet(name, lname) {
+    console.log("hello " + name, "", +lname);
 }
-        greet("Anhal","Ahmed");
-       
-        //calculating function
-        function square(number){
-            return number * number;
-        }
+greet("Anhal", "Ahmed");
+
+//calculating function
+function square(number) {
+    return number * number;
+}
 // let calculate = square(2);
 
-        console.log(square(2));
+console.log(square(2));
 
 /*=====  End of lesson6  ======*/
-
