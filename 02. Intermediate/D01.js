1cdfd6f4fc51836c4514837243b1c11c83d92aa1
codefromhome1 <fatema14384@gmail.com>
2025-01-26 19:58:15 +0300

    Done

---
+++ 02. Intermediate/D01.js
@@ -0,0 +1,40 @@
+
+/*=============================================
+=            Day 1 JS Studying            =
+=============================================*/
+
+
+/**
+ *
+ * Variables
+ *
+ */
+
+
+// Auto declare
+
+num1 = 30;
+num2=20;
+num = num1+num2;
+console.log(num);
+
+// declare using letterSpacing: 
+let a = 'Ibtisam';
+let b = 'Ali';
+console.log(a + " " +b);
+
+// Declare using Var
+
+var var1 = 'Hello';
+var var2 = "World!";
+
+console.log(var1 + " " + var2);
+
+
+// Declare const
+
+const numbers = 20+40;
+console.log(numbers); 
+
+/*=====  End of Section comment block  ======*/
+
