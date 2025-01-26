4a9e79aefd61b6d9a32f7381c8acaf22454dca80
codefromhome1 <fatema14384@gmail.com>
2025-01-26 12:02:50 +0300

    Day 1 Studying JavaScript

--- 02. Intermediate/practice/Test.js
+++ 02. Intermediate/practice/Test.js
@@ -2,9 +2,32 @@
 =            Test 1            =
 =============================================*/
 
-for (let k = 0; k <= 10; k++) {
-    console.log(k);
-}
+
+/**
+ *
+ * Day 1 Studying JavaScript
+ *
+ */
+
+/*=====  Start of Day 1 Studying JavaScript  ======*/
+
+var x ,y;
+x = 10+10;
+y = x+10;
+// y =x;
+
+console.log(x);
+console.log(y);
+
+/*=====  End of Day 1 Studying JavaScript  ======*/
+
+
+
+
+
+// for (let k = 0; k <= 10; k++) {
+//     console.log(k);
+// }
 
 
 /*=====  End of Test  ======*/
