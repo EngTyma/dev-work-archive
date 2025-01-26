4d7e8ad9e6aecede445a6850023f47c04fc302ca
codefromhome1 <fatema14384@gmail.com>
2025-01-26 16:48:59 +0300

    Variables

---
+++ 02. Intermediate/practice/D01-Variables.js
@@ -0,0 +1,32 @@
+
+/*=============================================
+=            Day 1 Studying JavaScript            =
+=============================================*/
+
+/**
+ * Variables
+ */
+
+ /*=====  D01-Variables.js  ======*/
+
+ // Variables are containers for storing data
+ /**
+ * JavaScript Variables can be declared in 4 ways
+ */
+//  Automatically
+ // Variables are declared using the var keyword
+ // Variables are declared using the let keyword
+ // Variables are declared using the const keyword
+ 
+ var x = 10;
+ let y = 20;
+ const z = 30;
+ 
+ console.log(x);
+ console.log(y);
+ console.log(z);
+
+ /*=====  End of Section comment block  ======*/
+
+ /*=====  Start of Day 2 Studying JavaScript  ======*/
+
