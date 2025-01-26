f8b87660880d9c61768b78782f52961b8749c145
codefromhome1 <fatema14384@gmail.com>
2025-01-26 16:55:33 +0300

    Variables

--- 02. Intermediate/practice/D01-Variables.js
+++ 02. Intermediate/practice/D01-Variables.js
@@ -18,15 +18,34 @@
  // Variables are declared using the let keyword
  // Variables are declared using the const keyword
  
+ 
+ /* 1= automatically declares a variable */
+ a =2;
+ b=3;
+ console.log(a);
+ console.log(b);
+ 
+ /* 2= declares a variable using the var keyword */
  var x = 10;
  let y = 20;
- const z = 30;
+ const z = x + y;
  
  console.log(x);
  console.log(y);
  console.log(z);
 
- /*=====  End of Section comment block  ======*/
+ /* 3= declares a variable using the let keyword */
+ let var1 = 10;
+ let var2 = 20;
+ let var3 = var1 + var2;
+ console.log(var3);
+ 
+ /* 4= declares a variable using the const keyword */
+ const num1 = 10;       
+ const num2 = 20;
+ const sum = num1 + num2;
+ console.log(sum);
 
- /*=====  Start of Day 2 Studying JavaScript  ======*/
+ /*=====  End of D01-Variables.js  ======*/
+
 
