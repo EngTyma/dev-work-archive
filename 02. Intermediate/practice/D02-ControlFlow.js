06dc4dd7d6533f98bfe646cee753df565e79b9e6
codefromhome1 <fatema14384@gmail.com>
2025-01-31 19:24:42 +0300

    done

---
+++ 02. Intermediate/practice/D02-ControlFlow.js
@@ -0,0 +1,49 @@
+/*=============================================
+=            Control Flow Practice            =
+=                                              =
+=  This is a practice file for the Control    =
+=  Flow section of the Zero to Full Stack     =
+=  JavaScript Course.  Feel free to          =
+=  change this file for practice.             =
+=============================================*/
+
+let age = prompt("What is your age?");
+
+if (age >= 18) {
+    console.log("You are an adult.");
+} else {
+    console.log("You are a minor.");
+}
+
+let age = prompt("What is your age?");
+
+if (age >= 18) {
+    console.log("You are an adult.");
+} else if (age >= 13) {
+    console.log("You are a teenager.");
+} else {
+    console.log("You are a minor.");
+}
+
+let age = prompt("What is your age?");
+
+if (age >= 18) {
+    console.log("You are an adult.");
+} else if (age < 13) {
+    console.log("You are a minor.");
+} else {
+    console.log("You are a teenager.");
+}
+
+let age = prompt("What is your age?");
+
+if (age >= 18) {
+    console.log("You are an adult.");
+} else if (age < 13) {
+    console.log("You are a teenager.");
+} else {
+    console.log("You are a minor.");
+}
+
+
+/*=====  End of Section comment block  ======*/
