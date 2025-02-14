59ce493a33290489ee8ad31f212790dc74c94bcf
codefromhome1 <fatema14384@gmail.com>
2025-02-14 14:04:33 +0300

    Done

--- 02. Intermediate/practice/D04-Function.js
+++ 02. Intermediate/practice/D04-Function.js
@@ -6,36 +6,60 @@ function sayHello() {
     alert("Hello World")
 }
 
-// function declaration
 function add(a, b) {
-    return a + b;
+    return a + b; // Function-ka wuxuu soo celinayaa isku darka
 }
 
-console.log(add(2, 3));
+function handleClick() {
+    let result = add(2, 3); // Wacitaanka function-ka
+    alert(result); // Muujinta natiijada
+}
+
+
+
+// console.log(add(2, 3));
 
 function subtract(a, b) {
     return a - b;
 }
 
-console.log(subtract(5, 3));
+function subClick() {
+    let result = subtract(5, 3);
+    alert(result);
+}
+
+// console.log(subtract(5, 3));
 
 function multiply(a, b) {
     return a * b;
 }
 
-console.log(multiply(2, 3));
+function multiplyClick() {
+    let result = multiply(2, 3);
+    alert(result);
+}
+// console.log(multiply(2, 3));
 
 function divide(a, b) {
     return a / b;
 }
 
-console.log(divide(5, 3));
+function divideClick() {
+    let result = divide(5, 2)
+    alert(result)
+}
+// console.log(divide(5, 3));
 
 function power(a, b) {
     return a ** b;
 }
 
-console.log(power(2, 3));
+function multiplyClick() {
+    let result = power(4, 4)
+    alert(result)
+}
+
+// console.log(power(2, 3));
 
 
 
