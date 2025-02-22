d7494bb82a2949a8d33f1f31b2c7c63d22c3014e
codefromhome1 <fatema14384@gmail.com>
2025-02-22 23:14:46 +0300

    done

--- 02. Intermediate/Basics/day1.js
+++ 02. Intermediate/Basics/day1.js
@@ -17,5 +17,17 @@ const person = {
     name: "fatema",
     age: 23,
 }
-
 console.log(person.name + " " + person.age)
+
+//click me
+
+let a = 10,
+    b = 10;
+
+
+function add() {
+    let c = a + b
+        // document.getElementsByClassName(text).innerText = c;
+    document.getElementsByClassName('text')[0].innerText = c;
+
+}
