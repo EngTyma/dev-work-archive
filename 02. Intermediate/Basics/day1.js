fb81a255a5bf370a38328b1414bee605cc8ac847
codefromhome1 <fatema14384@gmail.com>
2025-02-22 23:54:18 +0300

    Done

--- 02. Intermediate/Basics/day1.js
+++ 02. Intermediate/Basics/day1.js
@@ -21,13 +21,17 @@ console.log(person.name + " " + person.age)
 
 //click me
 
-let a = 10,
-    b = 10;
+// let a = 10,
+//     b = 10;
 
+
+let a, b;
 
 function add() {
     let c = a + b
+        // let d = ' ';
         // document.getElementsByClassName(text).innerText = c;
-    document.getElementsByClassName('text')[0].innerText = c;
+    document.getElementsByClassName('add-input')[0].value = c;
+    // document.getElementsByClassName('text')[0].innerText = c;
 
 }
