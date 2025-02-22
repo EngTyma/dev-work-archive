069990a963d7d890157dc3521dea6b7508c2d1d0
codefromhome1 <fatema14384@gmail.com>
2025-02-22 22:49:09 +0300

    Done

--- 02. Intermediate/Basics/day1.js
+++ 02. Intermediate/Basics/day1.js
@@ -1,9 +1,16 @@
 // 
 
-console.log("hello world");
+// console.log("hello world");
 
-var name = "zero to fullstack";
-console.log(name);
+// var name = "zero to fullstack";
+// console.log(name);
 
-var age = 23;
-console.log(age);
+// var age = 23;
+// console.log(age);
+
+const person = {
+    name: "fatema",
+    age: 23,
+}
+
+console.log(person.name + " " + person.age)
