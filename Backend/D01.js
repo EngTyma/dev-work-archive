985e71eb463fce7316e9849316335756f9216585
codefromhome1 <fatema14384@gmail.com>
2025-02-25 21:17:56 +0300

    Done

--- Backend/D01.js
+++ Backend/D01.js
@@ -12,3 +12,8 @@ let hername = "Alice";
 let herage = 25;
 
 console.log("hello my name is " + hername + " I'm  " + age + " years old");
+
+let myname = "fatma";
+let myage = 23;
+
+console.log("hello my name is " + myname + " I'm  " + myage + " years old");
