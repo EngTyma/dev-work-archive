e1ce8a831cf98c72865573ffecf53bdee9f5a248
EngTyma <fatema14384@gmail.com>
2025-11-24 18:30:17 +0300

    Done

--- test_card/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./index.html", "./*.html", "./**/*.js", "./**/*.ts"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
