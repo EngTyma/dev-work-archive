51ebe5ab5b5c20461d3c8c184d53aa66c1eb906a
EngTyma <fatema14384@gmail.com>
2025-12-08 17:13:45 +0300

    Done

--- my-tailwind-project/navbar/script.js
+++
@@ -1,5 +0,0 @@
-const btnActive = document.querySelector("#active");
-const navbar = document.querySelector(".navbar");
-btnActive.addEventListener("click", () => {
-  navbar.classList.toggle("hidden");
-});
--- my-tailwind-project/sideBarNav/script.js
+++
@@ -1,5 +0,0 @@
-const active = document.getElementById("active");
-const aside = document.querySelector(".aside");
-active.addEventListener("click", () => {
-  aside.classList.toggle("hidden");
-});
